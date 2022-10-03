046934cb5a1e3f921f09752b226f3c04aff44e11
Pierce Nyamuda <ptnrlab@gmail.com>
2022-10-03 18:46:16 +0200

    welcome  message changed to lowercase

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 module.exports = require("./quotes");
 
 module.exports.homeView = function(req, res) {
-    return res.send("WELCOME TO THE BEST QUOTES API");
+    return res.send("Welcome to the best quotes API");
 }
