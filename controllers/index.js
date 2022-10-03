60c49056515367fd3674ac340ab6af129eed4fa4
Pierce Nyamuda <ptnrlab@gmail.com>
2022-10-03 12:01:16 +0200

    welcome message changed

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 module.exports = require("./quotes");
 
 module.exports.homeView = function(req, res) {
-    return res.send("WELCOME TO QUOTES APP");
+    return res.send("WELCOME TO THE BEST QUOTES API");
 }
